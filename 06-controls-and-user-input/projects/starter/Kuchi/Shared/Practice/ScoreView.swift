caccb35f006db0dce55b7bc125280a40cc17a24d
🤨 <jorge_mz@icloud.com>
2021-04-10 10:23:29 -0500

    - add ScoreView - simulate State logic

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Practice/ScoreView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Practice/ScoreView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -31,43 +31,41 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import os
 
-struct ChallengeView: View {
-  let challengeTest: ChallengeTest
-  @State var showAnswers = false
+class Box<T> {
+  var wrappedValue: T
+  init(initialValue value: T) { self.wrappedValue = value }
+}
+
+struct ScoreView: View {
+  let logger = Logger(subsystem: "com.dserweb.scoreView", category: "scoreview")
+  var numberOfQuestions = 5
+  
+  struct State {
+    var numberOfAnswered = Box<Int>(initialValue: 0)
+  }
+  
+  var state = State()
   
   var body: some View {
-    VStack {
-      Button(action: {
-        self.showAnswers.toggle()
-      }) {
-        QuestionView(question: challengeTest.challenge.question)
-          .frame(height: 300)
+    Button(action: {
+      self.state.numberOfAnswered.wrappedValue += 1
+      logger.info("Answered: \(self.state.numberOfAnswered.wrappedValue)")
+    }, label: {
+      HStack {
+        Text("\(state.numberOfAnswered.wrappedValue)/\(numberOfQuestions)")
+          .font(.caption)
+          .padding(4)
+        Spacer()
       }
-      ScoreView()
-      if showAnswers {
-        Divider()
-        ChoicesView(challengeTest: challengeTest)
-          .frame(height: 300)
-          .padding()
-      }
-    }
+    })
   }
   
 }
 
-struct ChallengeView_Previews: PreviewProvider {
-  static let challengeTest = ChallengeTest(
-    challenge: Challenge(
-      question: "おねがい　します",
-      pronunciation: "Onegai shimasu",
-      answer: "Please"
-    ),
-    answers: ["Thank you", "Hello", "Goodbye"]
-  )
-  
+struct ScoreView_Previews: PreviewProvider {
   static var previews: some View {
-    return ChallengeView(challengeTest: challengeTest)
+    ScoreView()
   }
-  
 }
