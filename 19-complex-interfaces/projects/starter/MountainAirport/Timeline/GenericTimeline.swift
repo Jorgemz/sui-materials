25c47012a57c27ffe01bfc58d451a548637d1971
🤨 <jorge_mz@icloud.com>
2021-05-24 11:21:34 -0500

    - new View Generic for use easy

--- 19-complex-interfaces/projects/starter/MountainAirport/Timeline/GenericTimeline.swift
+++ 19-complex-interfaces/projects/starter/MountainAirport/Timeline/GenericTimeline.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,42 +28,36 @@
 
 import SwiftUI
 
-struct TimelineView: View {
-  var flights: [FlightInformation]
+struct GenericTimeline<Content: View>: View {
+  let flights: [FlightInformation]
+  let content: (FlightInformation) -> Content
 
+  // 2
+  init(
+    flights: [FlightInformation],
+    @ViewBuilder content: @escaping (FlightInformation) -> Content
+  ) {
+    self.flights = flights
+    self.content = content
+  }
+
+  // 3
   var body: some View {
-    ZStack {
-      Image("background-view")
-        .resizable()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-      ScrollView {
-        VStack {
-          GenericTimeline(flights: flights) { flight in
-            FlightCardView(flight: flight)
-          }
+    ScrollView {
+      VStack {
+        ForEach(flights) { flight in
+          content(flight)
         }
       }
-      .padding()
     }
-    .foregroundColor(.white)
-    .navigationTitle("Flight Timeline")
   }
 }
 
-struct TimelineView_Previews: PreviewProvider {
-  static var previews: some View {
-    NavigationView {
-      TimelineView(
-        flights: FlightData.generateTestFlights(
-          date: Date()
-        )
-        .filter {
-          Calendar.current.isDate(
-            $0.localTime,
-            inSameDayAs: Date()
-          )
-        }
-      )
+struct GenericTimeline_Previews: PreviewProvider {
+    static var previews: some View {
+      GenericTimeline(flights: FlightData.generateTestFlights(date: Date())) {
+        flight in
+        FlightCardView(flight: flight)
+      }
     }
-  }
 }
