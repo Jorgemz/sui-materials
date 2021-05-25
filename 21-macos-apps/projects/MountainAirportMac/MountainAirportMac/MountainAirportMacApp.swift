44c54aceed342d0a73ac5a51660129047410ff84
jorgemz <jorge_mz@icloud.com>
2021-05-25 11:15:26 -0500

    - migration iOS to mac - fix issues when build app

---
+++ 21-macos-apps/projects/MountainAirportMac/MountainAirportMac/MountainAirportMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountainAirportMacApp.swift
+//  MountainAirportMac
+//
+//  Created by 🤨 on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MountainAirportMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
